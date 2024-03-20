f8f502222362822bb5a675e2494dcb29b77f7882
Thineswar <75237282+Thineswar@users.noreply.github.com>
2024-03-20 14:53:42 +0800

    Added Worker
    Body responds as expected. Country flag hyperlink to come.

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,39 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { parse } from "cookie";
+import jwt from '@tsndr/cloudflare-worker-jwt';
 
 export default {
-  async fetch(request, env, ctx) {
-    return new Response('Hello World!');
+  async fetch(request) {
+
+    function getCookie(name){
+      // Get CF_Authorization JWT token stored as cookie
+      const cookie = parse(request.headers.get("Cookie") || "");
+      if (cookie[name] != null) {
+        return jwt.decode(cookie[name]);
+      }
+    }
+
+    const parsedJwt = getCookie("CF_Authorization")
+
+    const html = `
+    <!DOCTYPE html>
+    <head>
+      <title>Cloudflare TVE Technical Project</title>
+    </head>
+		<body>
+      <header>
+        <h1>Cloudflare TVE Technical Project</h1>
+      </header>
+      <p>
+        ${parsedJwt.payload.email} authenticated at ${Date(parsedJwt.payload.iat)} from ${parsedJwt.payload.country}.
+      </p>
+		</body>
+    <footer> <b> By Thineswar </b> </footer>
+    `;
+
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    });
   },
 };
